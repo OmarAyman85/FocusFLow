db819a715736810c6a71d7f21e56bb89dc4d85d9
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-18 22:44:27 +0200

    Initializing the server and the structure

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { ClientSession } from "mongodb";
+
+//environment variables
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Focus Flow Backend is running!");
+});
+
+//Initialize the database and the server
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
